714f766cdd9104c507356978d546eb36ba19e99b
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-07-04 19:50:35 -0500

    Firebase Integration

--- app/src/main/java/com/example/proyecto02_danp/MainActivity.kt
+++ app/src/main/java/com/example/proyecto02_danp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.proyecto02_danp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        db.collection("endangered_animals")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.e("Objeto", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Error", "Error getting documents.", exception)
+            }
     }
 }
